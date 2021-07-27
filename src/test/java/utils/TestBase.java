ee43a30427556db8787ac36f5b498446928be595
annashv <annashivrina@gmail.com>
2021-07-27 19:40:59 +0200

    Added listener

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -1,6 +1,8 @@
 package utils;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 ;
 
@@ -8,6 +10,7 @@ public class TestBase {
 
     @BeforeAll
     public static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
     }

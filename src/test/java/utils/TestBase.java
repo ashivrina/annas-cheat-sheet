8da03d130a64a902df3911975b69d2c4dfd481ed
annashv <annashivrina@gmail.com>
2021-07-27 20:10:13 +0200

    Added VNC capability

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -4,13 +4,18 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 ;
 
 public class TestBase {
 
     @BeforeAll
     public static void setUp() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         Configuration.baseUrl = "https://demoqa.com";

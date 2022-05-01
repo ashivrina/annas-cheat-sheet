e6d4f0acf54482ba1f3348a23ed129a72cf465c0
annashv <annashivrina@gmail.com>
2022-05-01 10:57:16 +0200

    Search test

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -3,7 +3,6 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTest {
